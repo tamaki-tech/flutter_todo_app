99ef41f64f742f55d0f4caba7d6809834429b547
tamaki_tech <oshaburimegane@gmail.com>
2023-02-19 18:15:09 +0900

    アプリ立ち上がらないバグを修正

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // TODO 何者なのか調べる
   final dir = await getApplicationSupportDirectory();
   final isar = await Isar.open([TodoSchema], directory: dir.path);
 
